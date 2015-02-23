600cb613b12295cd1ff411048a4fa689d322102d
Yu-Hsin Sean Wang <katnapped@hotmail.com>
2015-02-22 16:21:09 -0800

    Enabled hooks. Setup jRating. Enabled Caboose.

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -37,6 +37,11 @@ class Application extends CI_Controller {
         // finally, build the browser page!
         $this->data['data'] = &$this->data;
         $this->parser->parse('_template', $this->data);
+        
+        $this->data['caboose_styles']    = $this->caboose->styles(); 
+        $this->data['caboose_scripts']   = $this->caboose->scripts(); 
+        $this->data['caboose_trailings'] = $this->caboose->trailings(); 
+
     }
 
 }
